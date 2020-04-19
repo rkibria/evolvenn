81faf35eb6aecb76d84b72cf941de5f34d9f97f7
Raihan Kibria <rkibria@users.noreply.github.com>
2020-04-19 12:32:47 +0100

    Store center pos

--- visualizer.js
+++ visualizer.js
@@ -4,32 +4,36 @@ function Visualizer(model, x, y, s) {
 	this.x = x;
 	this.y = y;
 	this.s = s;
+
+	this.center = new Vec2(this.x + this.s/2, this.y + this.s/2);
 }
 
-Visualizer.prototype.draw = function(ctx) {
+Visualizer.prototype.draw = function(ctx, accel=null) {
 	ctx.save();
 
 	// Center cross
 	ctx.strokeStyle = "darkgrey";
 	ctx.beginPath();
 	const crossSize = 10;
-	const centerX = this.x + this.s/2;
-	const centerY = this.y + this.s/2;
-	ctx.moveTo(centerX - crossSize, centerY);
-	ctx.lineTo(centerX + crossSize, centerY);
+	ctx.moveTo(this.center.x - crossSize, this.center.y);
+	ctx.lineTo(this.center.x + crossSize, this.center.y);
 	ctx.stroke(); 
-	ctx.moveTo(centerX, centerY - crossSize);
-	ctx.lineTo(centerX, centerY + crossSize);
+	ctx.moveTo(this.center.x, this.center.y - crossSize);
+	ctx.lineTo(this.center.x, this.center.y + crossSize);
 	ctx.stroke(); 
 
 	// Frame
 	ctx.strokeRect(this.x, this.y, this.s, this.s);
 
+	// Acceleration indicator
+	if(accel != null) {
+	}
+
 	// Particle
 	ctx.fillStyle = "red";
 	ctx.beginPath();
-	const x = centerX + Math.trunc(this.model.particle.pos.x);
-	const y = centerY - Math.trunc(this.model.particle.pos.y);
+	const x = this.center.x + Math.trunc(this.model.particle.pos.x);
+	const y = this.center.y - Math.trunc(this.model.particle.pos.y);
 	const r = 3;
 	ctx.arc(x, y, r, 0, (Math.PI * 2), true);
 	ctx.closePath();
