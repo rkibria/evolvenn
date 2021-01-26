4401fdeb3486228b5c356d726814cb340b607e21
Raihan Kibria <rkibria@users.noreply.github.com>
2021-01-26 20:05:14 +0000

    draw explosion

--- fuelpods/visualizer.js
+++ fuelpods/visualizer.js
@@ -5,6 +5,36 @@
 	The drawing method does not automatically run the model.
 */
 
+function drawStar(ctx, cx, cy, spikes, outerRadius, innerRadius) {
+	ctx.save();
+	var rot = Math.PI / 2 * 3;
+	var x = cx;
+	var y = cy;
+	var step = Math.PI / spikes;
+
+	ctx.beginPath();
+	ctx.moveTo(cx, cy - outerRadius)
+	for (i = 0; i < spikes; i++) {
+		x = cx + Math.cos(rot) * outerRadius;
+		y = cy + Math.sin(rot) * outerRadius;
+		ctx.lineTo(x, y)
+		rot += step
+
+		x = cx + Math.cos(rot) * innerRadius;
+		y = cy + Math.sin(rot) * innerRadius;
+		ctx.lineTo(x, y)
+		rot += step
+	}
+	ctx.lineTo(cx, cy - outerRadius)
+	ctx.closePath();
+	ctx.lineWidth=5;
+	ctx.strokeStyle='red';
+	ctx.stroke();
+	ctx.fillStyle='yellow';
+	ctx.fill();
+	ctx.restore();
+}
+
 // x,y = top left, s = edge length of square display
 function Visualizer(model, x, y, s) {
 	this.model = model;
@@ -280,4 +310,13 @@ Visualizer.prototype.drawPath = function(ctx, positions, color="white") {
 	}
 	ctx.stroke();
 	ctx.restore();
+}
+
+Visualizer.prototype.drawExplosion = function(ctx, radius) {
+	const p = this.model.rocket;
+	const dx = Math.trunc(p.pos.x);
+	const dy = Math.trunc(p.pos.y);
+	const x = this.center.x + dx;
+	const y = this.center.y - dy;
+	drawStar(ctx, x, y, 11, radius, Math.trunc(radius / 6));
 }
